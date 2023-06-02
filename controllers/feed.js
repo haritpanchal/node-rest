0c7cc40f20a32f979399d1409b0c7494be130e9f
Harit Panchal <panchalharit007@gmail.com>
2023-06-02 18:18:05 +0530

    feed list and create api created

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,16 +1,35 @@
+const {} = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
-  return res.status(200).json({
-    posts: [{ title: "First post", content: "This is node REST API response" }],
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First post",
+        content: "This is node REST API response",
+        imageUrl: "images/bulb.jpeg",
+        creator: {
+          name: "Harit",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-
+  console.log(req.body);
   // create
   res.status(201).json({
     message: "Post created successfully.",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Harry" },
+      createdAt: new Date(),
+    },
   });
 };
